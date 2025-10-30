b26408ee408a4ed8e48f598474629010fe973dae
lenovo <lenovo@AdhiRaghav>
2025-10-30 12:46:52 +0530

    commit

--- Assignments/src/Assignment2/Student.java
+++ Assignments/src/Assignment2/Student.java
@@ -4,7 +4,11 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		public class Student {
+			public Student() {
+		        System.out.println("Student object is created");
+		    }
+		}
 	}
 
 }
