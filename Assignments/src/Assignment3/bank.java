e9a8fa5b4bb659650ecd941b19737bd06ced36ae
lenovo <lenovo@AdhiRaghav>
2025-10-30 12:53:12 +0530

    commit

---
+++ Assignments/src/Assignment3/bank.java
@@ -0,0 +1,20 @@
+package Assignment3;
+
+public class bank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class Bank {
+		    private static int totalAccounts = 0;
+
+		    public static void incrementAccountCount() {
+		        totalAccounts++;
+		    }
+
+		    public static int getTotalAccounts() {
+		        return totalAccounts;
+		    }
+		}
+	}
+
+}
