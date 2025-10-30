4117e58befa18b60938e7fd9fb382dab068322ef
lenovo <lenovo@AdhiRaghav>
2025-10-30 13:00:38 +0530

    commit

---
+++ Assignments/src/Assignment4/AirFare.java
@@ -0,0 +1,13 @@
+package Assignment4;
+
+public class AirFare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public interface Airfare {
+		    Double calculateAmount(); // abstract method
+		    void display();            // method to display details
+		}
+	}
+
+}
