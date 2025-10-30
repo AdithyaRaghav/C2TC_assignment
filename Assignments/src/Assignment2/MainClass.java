e7c0716deaf8d0b3beae9b84c1b7e8e0ae636f62
lenovo <lenovo@AdhiRaghav>
2025-10-30 12:45:23 +0530

    commit

---
+++ Assignments/src/Assignment2/MainClass.java
@@ -0,0 +1,20 @@
+package Assignment2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		public class MainClass {
+		    public static void main(String[] args) {
+		        // Demonstrating Student class
+		        Student s = new Student();
+
+		        // Demonstrating Commission class
+		        Commision emp = new Commision();
+		        emp.acceptDetails();
+		        emp.calculateCommission();
+		    }
+		}
+	}
+
+}
